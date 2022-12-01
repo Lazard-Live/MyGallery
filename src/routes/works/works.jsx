d1e89c9ad6b457d8ea85fece64798aec71fefd6a
DoretskiyBV <63367508+Lazard-Live@users.noreply.github.com>
2022-12-01 14:29:01 +0300

    Исправил works.scss и works.jsx

--- src/routes/works/works.jsx
+++ src/routes/works/works.jsx
@@ -2,11 +2,11 @@ export default function Works() {
     return (
         <div className="works">
             <div className="row works__row works__row_content-center">
-                <div className="col col-sm-12 col-md-6 col-lg-3 col-xl-3 works__col">
+                <div className="col">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, laboriosam.
                 </div>
-                <div className="col col-sm-12 col-md-6 col-lg-3 col-xl-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, explicabo.</div>
-                <div className="col col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                <div className="col">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, explicabo.</div>
+                <div className="col">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, suscipit tempora.
                 </div>
             </div>
