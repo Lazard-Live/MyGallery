88f53c9ad8a81f5e2692cec17c02047bbb02f729
DoretskiyBV <63367508+Lazard-Live@users.noreply.github.com>
2022-12-06 20:40:54 +0300

    Добавил аватар, текст и стили для about

--- src/routes/about/about.jsx
+++ src/routes/about/about.jsx
@@ -1,21 +1,17 @@
 export default function info() {
     const info = {
-        first: 'Богдан',
-        last: 'Дорецкий',
-        avatar: 'https://imgholder.ru/200x200/2D3F50/F4F8F9&text=Заглушка&font=kelson',
-        notes: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium ad animi aspernatur blanditiis consectetur consequuntur distinctio dolor doloremque eligendi error eum, facere facilis inventore ipsum iure labore laudantium libero nesciunt nihil odio odit officiis optio pariatur perferendis possimus quam quas quis quo repellendus reprehenderit repudiandae ullam veniam veritatis voluptatibus!',
-        favorite: true,
+        firstName: 'Богдан',
+        lastName: 'Дорецкий',
+        avatarImg: './img/about/face.jpg',
+        notes: 'Попал в ИТ более 10 лет назад. Прошёл путь от "эникейщика" до системного администратора. Хочу изучить JS и сопутствующие фреймворки для релокации в другие города и профессионального роста. В последние годы работал по другой специальности. Коммерческого опыта нет. Данное направление изучаю самостоятельно и совмещаю с основной работой.',
     };
 
     return (
-        <div className={'about'}>
-            <div className={'about_img-wrap'}>
-                <img key={info.avatar} src={info.avatar || null} />
-            </div>
-
-            <div>
-                <h1 className={'about_title'}>{info.first + ' ' + info.last}</h1>
-                <p className={'about_text'}>{info.notes}</p>
+        <div className='about'>
+            <div className='about__info'>
+                <img className='about__img' src={info.avatarImg} alt='Моя фотография'/>{' '}
+                <h1 className='about__title'>{info.firstName + ' ' + info.lastName}</h1>
+                <p className='about__text'>{info.notes}</p>
             </div>
         </div>
     );
