dbd6897e9993595d6390920d9af777e6196c18f0
DoretskiyBV <63367508+Lazard-Live@users.noreply.github.com>
2022-12-08 17:00:20 +0300

    Добавил контакты

--- src/routes/contacts/contacts.jsx
+++ src/routes/contacts/contacts.jsx
@@ -1,7 +1,17 @@
 export default function Contacts() {
     return (
-        <div id="contact">
-            <div>Тут Контакты</div>
+        <div className="contacts">
+            <div className="row contacts_row">
+                <div className="col-12 contacts_info">
+                    <b>E-MAIL</b>: <a href="mailto:workbd@bk.ru">WorkDB@BK.RU</a>
+                </div>
+                <div className="col-12 contacts_info">
+                    <b>Phone</b>: <a href="tel: +789633633517">8-963-363-35-17</a>
+                </div>
+                <div className="col-12 contacts_info">
+                    <b>Telegram</b>: <a href="https://t.me/workbd">@WorkBD</a>
+                </div>
+            </div>
         </div>
     );
 }
